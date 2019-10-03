dede830c74a68bf5768cb4d0683df7f3cabefab3
Christopher Gozdor <christopher.gozdor@gm.com>
2019-10-03 09:44:45 -0400

    small changes

--- DummyApplication/app/src/main/java/com/example/dummyapplication/MainActivity.java
+++ DummyApplication/app/src/main/java/com/example/dummyapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
         final EditText userName = findViewById(R.id.usernameTextInput);
         final EditText password = findViewById(R.id.passwordTextInput);
+        final TextView signUp = findViewById(R.id.signUpSwitch);
+        userName.setHint("User Name");
+        password.setHint("Password");
+        signUp.setText("Sign Up");
+        loginButton.setText("Login");
         loginButton.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
                 new AlertDialog.Builder(v.getContext())
@@ -25,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
 
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle(userName.getText() + " signUp")
+                        .setMessage(password.getText() + " signUp")
+                        .show();
+            }
         });
     }
 }
